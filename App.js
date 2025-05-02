a00aa98c5288f866e90fc75b2ef90c96a6d5217b
nikhilsharma9548 <ns630403@gmail.com>
2025-05-02 23:01:49 +0530

    committed

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ search.addEventListener('click', ()=> {
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature'); 
             const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity');
+            const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
             switch (json.weather[0].main){
